84868738a80495f656efcf9f84cd8ff9160138ad
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2022-07-28 10:55:06 -0400

    changing to fast env, not working yet

--- zero_sum_rewards.py
+++ zero_sum_rewards.py
@@ -16,7 +16,7 @@ def _closest_to_ball(state: GameState) -> Tuple[int, int]:
     blue_closest = -1
     orange_closest = -1
     for i, player in enumerate(state.players):
-        dist = np.linalg.norm(a - b for a, b in zip(player.car_data.position, state.ball.position))
+        dist = np.linalg.norm((a - b for a, b in zip(player.car_data.position, state.ball.position)))
         dist_list[i] = dist
         if state.players[i].team_num == BLUE_TEAM and blue_closest == -1:
             blue_closest = i
@@ -148,8 +148,8 @@ class ZeroSumReward(RewardFunction):
                 #     player_self_rewards[i] += self.ball_touch_dribble_w
 
                 # acel_ball
-                vel_difference = abs(np.linalg.norm(a - b for a, b in zip(self.last_state.ball.linear_velocity,
-                                                                          self.current_state.ball.linear_velocity)))
+                vel_difference = abs(np.linalg.norm((a - b for a, b in zip(self.last_state.ball.linear_velocity,
+                                                                          self.current_state.ball.linear_velocity))))
                 player_rewards[i] += vel_difference / 4600.0
 
                 # jump touch
